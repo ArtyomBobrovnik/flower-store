4693712268046ac34d56d4bad997abb098605e68
a.bobrovnik <artyom.bobrovnik@gcore.lu>
2023-12-03 14:08:50 +0000

    Setup UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import { unstable_cache } from "next/cache";
+
 import { api } from "~/trpc/server";
+import { Button } from "~/ui/button";
 
 export default async function Home() {
-  const arrangmentVariation = await unstable_cache(
+  const arrangmentVariations = await unstable_cache(
     () => api.arrangmentVariation.all.query(),
     ["arrangment"],
     { revalidate: 86400 },
   )();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Hello world
-        </h1>
-        <p>{JSON.stringify(arrangmentVariation)}</p>
-      </div>
+    <main>
+      <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+        Hello world
+      </h1>
+      <Button>Test</Button>
+      <p>{JSON.stringify(arrangmentVariations)}</p>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,35 @@
-import { type Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 import { fontFamily } from "tailwindcss/defaultTheme";
-
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: ["./src/**/*.tsx"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")],
+};
